530fb0e36850a42b36bc19c42a7746d01432294f
albertoruibal <alberto@alonsoruibal.com>
2014-02-11 21:10:14 +0100

    Format

--- gwt/src/main/resources/jmini3d/gwt/fragment-shader.glsl
+++ gwt/src/main/resources/jmini3d/gwt/fragment-shader.glsl
@@ -17,11 +17,11 @@ void main(void) {
     vec4 fragmentColor = texture2D(sampler, vec2(vTextureCoord.s, vTextureCoord.t));
     gl_FragColor = vec4(mix(fragmentColor.rgb, vObjectColor, vObjectColorTrans) * vLighting, fragmentColor.a);
 
-	if (reflectivity > 0.0) {
-    	vec3 eyeDirection = normalize(-vPosition.xyz);
-    	vec3 lookup = reflect(eyeDirection, vNormal);
-    	vec4 cubeColor = textureCube(envMap, -lookup);
-    	
-		gl_FragColor.rgb = mix(gl_FragColor.rgb, cubeColor.rgb, reflectivity);
-	}
+    if (reflectivity > 0.0) {
+        vec3 eyeDirection = normalize(-vPosition.xyz);
+        vec3 lookup = reflect(eyeDirection, vNormal);
+        vec4 cubeColor = textureCube(envMap, -lookup);
+
+        gl_FragColor.rgb = mix(gl_FragColor.rgb, cubeColor.rgb, reflectivity);
+    }
 }
