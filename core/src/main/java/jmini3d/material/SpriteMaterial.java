ab21f0243a86beeb02241e9d63263f202e3a9b12
albertoruibal <alberto@alonsoruibal.com>
2014-10-05 19:58:22 +0200

    Added some additional constructors to SpriteMaterial

--- core/src/main/java/jmini3d/material/SpriteMaterial.java
+++ core/src/main/java/jmini3d/material/SpriteMaterial.java
@@ -1,9 +1,22 @@
 package jmini3d.material;
 
 import jmini3d.Blending;
+import jmini3d.Color4;
 import jmini3d.Texture;
 
 public class SpriteMaterial extends Material {
+
+	public SpriteMaterial() {
+		super();
+		setBlending(Blending.NormalBlending);
+		setLighting(false);
+	}
+
+	public SpriteMaterial(Color4 color) {
+		super(color);
+		setBlending(Blending.NormalBlending);
+		setLighting(false);
+	}
 
 	public SpriteMaterial(Texture texture) {
 		super(texture);
