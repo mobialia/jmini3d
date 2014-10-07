06aab5b216bb097b2a98d8ba94d69a22e9b1a3c5
albertoruibal <alberto@alonsoruibal.com>
2014-10-07 10:21:26 +0200

    Support changing the image in a texture

--- core/src/main/java/jmini3d/Texture.java
+++ core/src/main/java/jmini3d/Texture.java
@@ -7,4 +7,12 @@ public class Texture {
 	public Texture(String image) {
 		this.image = image;
 	}
+
+	// TODO unload previous image
+	public void setImage(String image) {
+		if (this.image == null || !this.image.equals(image)) {
+			this.image = image;
+			status = 0;
+		}
+	}
 }
