8184cb49b18b8f2446c40b847da1063341d0ff67
albertoruibal <alberto@alonsoruibal.com>
2014-10-13 22:37:30 +0200

    touchController->inputController

--- demo-android/src/main/java/jmini3d/android/demo/DemoActivity.java
+++ demo-android/src/main/java/jmini3d/android/demo/DemoActivity.java
@@ -6,10 +6,9 @@ import android.widget.LinearLayout;
 import jmini3d.android.Activity3d;
 import jmini3d.android.input.InputController;
 import jmini3d.demo.DemoSceneController;
-import jmini3d.input.TouchListener;
 
 public class DemoActivity extends Activity3d {
-	InputController touchController;
+	InputController inputController;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -17,20 +16,14 @@ public class DemoActivity extends Activity3d {
 		DemoSceneController sceneController = new DemoSceneController();
 		glSurfaceView3d.setSceneController(sceneController);
 		glSurfaceView3d.getRenderer3d().setLogFps(true);
-		setTouchListener(sceneController);
+		inputController = new InputController(glSurfaceView3d);
+		inputController.setTouchListener(sceneController);
 	}
 
 	public void onCreateSetContentView() {
 		setContentView(R.layout.main);
 		LinearLayout root = ((LinearLayout) findViewById(R.id.root));
 		root.addView(glSurfaceView3d);
-	}
-
-	public void setTouchListener(TouchListener listener) {
-		if (touchController == null) {
-			touchController = new InputController(glSurfaceView3d);
-		}
-		touchController.setTouchListener(listener);
 	}
 
 }
--- demo-gwt/src/main/java/jmini3d/gwt/demo/DemoEntryPoint.java
+++ demo-gwt/src/main/java/jmini3d/gwt/demo/DemoEntryPoint.java
@@ -3,11 +3,10 @@ package jmini3d.gwt.demo;
 import jmini3d.demo.DemoSceneController;
 import jmini3d.gwt.EntryPoint3d;
 import jmini3d.gwt.input.InputController;
-import jmini3d.input.TouchListener;
 
 public class DemoEntryPoint extends EntryPoint3d {
 
-	InputController touchController;
+	InputController inputController;
 
 	@Override
 	public void onModuleLoad() {
@@ -16,14 +15,8 @@ public class DemoEntryPoint extends EntryPoint3d {
 		DemoSceneController sceneController = new DemoSceneController();
 		canvas3d.setSceneController(sceneController);
 		canvas3d.getRenderer3d().setLogFps(true);
-		setTouchListener(sceneController);
-	}
-
-	public void setTouchListener(TouchListener listener) {
-		if (touchController == null) {
-			touchController = new InputController(canvas3d.getElement());
-		}
-		touchController.setTouchListener(listener);
+		inputController = new InputController(canvas3d.getElement());
+		inputController.setTouchListener(sceneController);
 	}
 
 }
