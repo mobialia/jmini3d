5b88f28b92d12083729ae25483c258fb518d9151
albertoruibal <alberto@alonsoruibal.com>
2016-06-06 11:29:04 +0200

    New setTextLine method to modify existing Objects with text lines

--- core/src/main/java/jmini3d/Font.java
+++ core/src/main/java/jmini3d/Font.java
@@ -84,6 +84,13 @@ public class Font {
 		return getTextLine(text, null);
 	}
 
+	/**
+	 * Returns a new Object3D with a text line
+	 *
+	 * @param text
+	 * @param fontMetrics This object is going to be modified with the text line size
+	 * @return
+	 */
 	public Object3d getTextLine(String text, Rect fontMetrics) {
 		int length = text.length();
 		SpriteGeometry geometry = new SpriteGeometry(text.length());
@@ -114,6 +121,50 @@ public class Font {
 			fontMetrics.bottom = lineHeight;
 		}
 		return new Object3d(geometry, new SpriteMaterial(textures.get(0)));
+	}
+
+	/**
+	 * Modifies an Object3D with a SpriteGeometry to put a text
+	 *
+	 * @param object3d
+	 * @param text
+	 * @param fontMetrics
+	 */
+	public void setTextLine(Object3d object3d, String text, Rect fontMetrics) {
+		int length = text.length();
+		SpriteGeometry geometry = (SpriteGeometry) object3d.geometry3d;
+
+		int i;
+		int x = 0;
+		int y = 0;
+		char oldChar = (char) -1;
+		for (i = 0; i < length; i++) {
+			char c = text.charAt(i);
+			CharSprite cs = chars.get(c);
+			if (cs == null) {
+				System.out.print("Character " + c + " not found in bitmap texture");
+				continue;
+			}
+			for (Kerning k : kernings) {
+				if (k.first == oldChar && k.second == c) {
+					x += k.amount;
+				}
+			}
+
+			geometry.setSprite(i, x + cs.xoffset, y + cs.yoffset, x + cs.xoffset + cs.width, y + cs.yoffset + cs.height, //
+					Utils.p(cs.x + 1, scaleW), Utils.p(cs.y + 1, scaleH), Utils.p(cs.x + cs.width + 1, scaleW), Utils.p(cs.y + cs.height + 1, scaleH));
+			x += cs.xadvance;
+			oldChar = c;
+		}
+		if (fontMetrics != null) {
+			fontMetrics.right = x;
+			fontMetrics.bottom = lineHeight;
+		}
+
+		while (i < geometry.spriteCount) {
+			geometry.hideSprite(i);
+			i++;
+		}
 	}
 
 	public void clear() {
